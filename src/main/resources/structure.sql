beffd9559fa3475a64e0280fc05d01e01f9fa640
Oxana Odegova <odegova.oksana@gmail.com>
2013-08-10 22:58:55 +0400

    creating structure

--- src/main/resources/structure.sql
+++ src/main/resources/structure.sql
@@ -1,6 +1,11 @@
 -- You can use this file to load seed data into the database using SQL statements
 --insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
 
-
+CREATE  TABLE Users (
+  id INT NOT NULL ,
+  name VARCHAR(45) NOT NULL ,
+  email VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (id) , 
+  CONSTRAINT uc_email UNIQUE (email));
   
    
