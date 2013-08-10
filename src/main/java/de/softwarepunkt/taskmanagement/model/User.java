5be53c08e9b2225ec1e7824df750df2deea3fd74
Vladislav Dmitruk <vladislav.dmitruk@gmail.com>
2013-08-10 21:21:50 +0400

    User class completed

--- src/main/java/de/softwarepunkt/taskmanagement/model/User.java
+++ src/main/java/de/softwarepunkt/taskmanagement/model/User.java
@@ -1,10 +1,57 @@
 package de.softwarepunkt.taskmanagement.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
 	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@NotNull
+	@Size(min = 1, max = 25)
+    @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")	
 	private String name;
+
+	@NotNull
+	@NotEmpty
+	@Email
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
