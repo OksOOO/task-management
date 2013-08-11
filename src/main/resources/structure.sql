cb5afb989ac79cb287e247e1071fe40dbda8c690
Oxana Odegova <odegova.oksana@gmail.com>
2013-08-11 17:18:35 +0400

    rename id in tables

--- src/main/resources/structure.sql
+++ src/main/resources/structure.sql
@@ -2,20 +2,20 @@
 --insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
 
 CREATE  TABLE users (
-  id INT NOT NULL ,
+  user_id INT NOT NULL ,
   name VARCHAR(45) NOT NULL ,
   email VARCHAR(255) NOT NULL ,
   PRIMARY KEY (id) , 
   CONSTRAINT c_email UNIQUE (email));
   
 CREATE  TABLE task_type (
-  id INT NOT NULL ,
+  task_type_id INT NOT NULL ,
   type VARCHAR(45) NOT NULL,
   PRIMARY KEY (id) , 
   CONSTRAINT c_type UNIQUE (type));
   
 CREATE  TABLE task (
-  id INT NOT NULL ,
+  task_id INT NOT NULL ,
   create_user_id INT NULL ,
   create_date DATETIME NULL ,
   recipient_user_id INT NULL ,
@@ -30,7 +30,7 @@ CREATE  TABLE task (
   CONSTRAINT c_type_id FOREIGN KEY (type_id) REFERENCES task_type (id));
   
   CREATE  TABLE attachment (
-  id INT NOT NULL,
+  attachment_id INT NOT NULL,
   file_content BLOB NULL,
   description VARCHAR(512) NULL ,
   PRIMARY KEY (id) );
