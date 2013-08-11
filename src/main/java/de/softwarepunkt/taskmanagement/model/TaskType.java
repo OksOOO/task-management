6524ea2d2298b728548cae7b2ef578c664ba6861
Vladislav Dmitruk <vladislav.dmitruk@gmail.com>
2013-08-11 15:54:06 +0400

    Added TaskType entity

---
+++ src/main/java/de/softwarepunkt/taskmanagement/model/TaskType.java
@@ -0,0 +1,53 @@
+package de.softwarepunkt.taskmanagement.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+public class TaskType {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@NotNull
+	@Size(min = 1, max = 25)
+    @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")	
+	private String type;
+	
+	@OneToMany(mappedBy="taskType", cascade=CascadeType.ALL)
+	private Set<Task> tasks;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Set<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(Set<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
+}
