5afc7946c613346dab61ccc2beea2dba70133a78
Oxana Odegova <odegova.oksana@gmail.com>
2013-08-11 15:39:35 +0400

    task_type

--- src/main/resources/structure.sql
+++ src/main/resources/structure.sql
@@ -1,11 +1,17 @@
 -- You can use this file to load seed data into the database using SQL statements
 --insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
 
-CREATE  TABLE Users (
+CREATE  TABLE users (
   id INT NOT NULL ,
   name VARCHAR(45) NOT NULL ,
   email VARCHAR(255) NOT NULL ,
   PRIMARY KEY (id) , 
   CONSTRAINT uc_email UNIQUE (email));
+  
+CREATE  TABLE task_type (
+  id INT NOT NULL ,
+  type VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id) , 
+  CONSTRAINT type UNIQUE (type));
   
    
